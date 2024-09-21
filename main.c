e4f469a0a4a42377a7d67c4e648c7f7d6ddb5076
MagicTINTIN <magictintin@proton.me>
2024-09-21 12:40:07 +0200

    feat: multithreading with pthread

--- main.c
+++ main.c
@@ -2,19 +2,50 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <pthread.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stbi_image_write.h"
 #include "raytracing.h"
 #include "scene.h"
+
+#define NUMBER_OF_THREADS 12
+
+typedef struct threadArgs
+{
+   int thID;
+   vec3 *origin;
+   Color *image;
+} threadArgs;
+
+void *rowThread(void *thArgsPtr)
+{
+   threadArgs *thArgs = (threadArgs *)thArgsPtr;
+   for (int y = thArgs->thID; y < h; y+=NUMBER_OF_THREADS)
+   {
+      for (int x = 0; x < w; ++x)
+      {
+         vec3 dir = {(x - halfW) / (float)halfH, (y - halfH) / (float)halfH, 1}; // aspect ratio respected
+         // dir = minus(dir,origin);//fixed camera dir
+         dir = normalized(dir);
+         Ray ray = {*(thArgs->origin), dir};
+         rngState = x + y * w;
+         vec3 accumulatedColor = {0, 0, 0};
+         for (int i = 0; i < accumulationCount; ++i)
+            accumulatedColor = plus(accumulatedColor, times(calcColor(ray, maxBounce), 1. / accumulationCount));
+         thArgs->image[x + y * w] = vec3ToColor(accumulatedColor);
+      }
+   }
+   free(thArgs);
+}
 
 int main()
 {
    normalizedSunDirection = normalized(sunDirection);
    printf("Parsing triangles...\n");
    parseTriangleFile("triangles.txt");
-   //printAllTriangles();
-   Color image[w*h];
-   vec3 origin={0,-1,-1};
+   // printAllTriangles();
+   Color image[w * h];
+   vec3 origin = {0, -1, -1};
    /*
    vec3 dir={0,0,1};
    dir=normalized(dir);
@@ -22,22 +53,25 @@ int main()
    rngState = 0;
    calcColor(ray,3);
    */
-   for(int y=0;y<h;++y)
+   // for(int y=0;y<h;++y)
+   // {
+
+   // }
+   
+   pthread_t tIDs[NUMBER_OF_THREADS];
+   for (int i = 0; i < NUMBER_OF_THREADS; i++)
    {
-      for(int x=0;x<w;++x)
-      {
-         vec3 dir={(x-halfW)/(float)halfH,(y-halfH)/(float)halfH,1};//aspect ratio respected
-         //dir = minus(dir,origin);//fixed camera dir
-         dir = normalized(dir);
-         Ray ray = {origin,dir};
-         rngState = x+y*w;
-         vec3 accumulatedColor = {0,0,0};
-         for(int i=0;i<accumulationCount;++i)
-            accumulatedColor = plus(accumulatedColor,times(calcColor(ray,maxBounce),1./accumulationCount));
-         image[x+y*w]=vec3ToColor(accumulatedColor);
-      }
+      threadArgs *args = malloc(sizeof(threadArgs));
+      args->thID = i;
+      args->origin = &origin;
+      args->image = image;
+      pthread_create(&tIDs[i], NULL, rowThread, args);
+   }
+   for (int i = 0; i < NUMBER_OF_THREADS; i++)
+   {
+      pthread_join(tIDs[i], NULL);
    }
    stbi_write_bmp("out.bmp",w,h,3,(void const*)image);
-   
+
    return 0;
 }
