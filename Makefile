1038e280a3827a952b24d5041c670a79adfa1ea1
Atsuyo64 <victorlasserre78@gmail.com>
2024-09-21 13:06:59 +0200

    better Makefile

--- Makefile
+++ Makefile
@@ -11,25 +11,25 @@ all: opti
 
 # DEBUG BINS
 
-moremath_d.o: moremath.c moremath.h
-	$(CC) $(CFLAGS_DBG) -c moremath.c moremath.h
+moremath_d.o: moremath.c moremath.h scene.h
+	$(CC) $(CFLAGS_DBG) -c moremath.c
 
-raytracing_d.o: raytracing.c raytracing.h
-	$(CC) $(CFLAGS_DBG) -c raytracing.c raytracing.h
+raytracing_d.o: raytracing.c raytracing.h scene.h
+	$(CC) $(CFLAGS_DBG) -c raytracing.c
 
-main_d.o: main.c
+main_d.o: main.c scene.h
 	$(CC) $(CFLAGS_DBG) -c main.c
 
 
 # OPTI BINS
 
-moremath.o: moremath.c moremath.h
-	$(CC) $(CFLAGS_OPTI) -c moremath.c moremath.h
+moremath.o: moremath.c moremath.h scene.h
+	$(CC) $(CFLAGS_OPTI) -c moremath.c
 
-raytracing.o: raytracing.c raytracing.h
-	$(CC) $(CFLAGS_OPTI) -c raytracing.c raytracing.h
+raytracing.o: raytracing.c raytracing.h scene.h
+	$(CC) $(CFLAGS_OPTI) -c raytracing.c
 
-main.o: main.c
+main.o: main.c scene.h
 	$(CC) $(CFLAGS_OPTI) -c main.c
 
 
--- scene.h
+++ scene.h
@@ -4,6 +4,7 @@
 #include <math.h>
 #include "raytracing.h"
                                  //pos,radius,{color,emission}
+/*
 static Sphere const spheres[] = {{{2,-.8,2},1,{{1,1,1},0,1}},
                                  {{.51,-.3,2},.5,{{1,0,0},0,.75}},
                                  {{-1.5,.05,2},.4,{{0,1,0},0,.5}},
@@ -12,14 +13,16 @@ static Sphere const spheres[] = {{{2,-.8,2},1,{{1,1,1},0,1}},
                                  //{{-15,-15,10},15,{{1,.85,.85},1}},
                                  };
                                  //{{{0,0.7071068,4},1,{{1,1,1},1}},{{0,0,2},1,{{1,0,0},1}}};//debug
-static const int sphereCount = sizeof(spheres)/sizeof(Sphere);
+*/
+static Sphere const spheres[] = {{{0,0,0},0.5,{{1,1,1},10,0}},};
+static const int sphereCount = 0;//sizeof(spheres)/sizeof(Sphere);
 static Triangle *triangles=NULL; //to parse from triangles.txt
 static int triangleCount = 0;
 
-static int const w = 128*2;//width
-static int const h = 128*2;//height
-static int const accumulationCount = 100*1;//1000 = ok
-static int const maxBounce = 2;
+static int const w = 128*8;//width
+static int const h = 128*8;//height
+static int const accumulationCount = 100*9;//1000 = ok
+static int const maxBounce = 20;
 static vec3 const sunDirection = {-30,-85,100};
 static vec3 const SkyColorHorizon = {1,1,1};
 static vec3 const SkyColorZenith = {0.263,0.969,0.871};//{.5,.5,1};
