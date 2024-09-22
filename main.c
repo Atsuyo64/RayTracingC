d1ae847342eccefe7bc7b46c491cef31302cad2b
MagicTINTIN <magictintin@proton.me>
2024-09-22 17:31:15 +0200

    feat: flexible params

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 
 int width = 128;
 int height = 128;
+int maxBounce = 10;
+vec3 sunDirection = {-30,-85,100};
 
 typedef struct threadArgs
 {
@@ -25,6 +27,30 @@ typedef struct threadArgs
 #include <pthread.h>
 #define NUMBER_OF_THREADS 12
 
+// HELP SECTION WITH OVERLOAD
+
+void printHelp4(char const *progName, char const *errorDescription, char const *precision, char const *afterPrecision)
+{
+   fprintf(stderr, "%s%s%s", errorDescription, precision, afterPrecision);
+   printf("%s [-h|--help]\n\t[-i|--input path/to/file.obj]\n\t[-p|--pos <posX> <posY> <posZ>]\n\t[-t|--track <trackX> <trackY> <trackZ>]\n\t[-f|--fov <fov>]\n\t[-s|--size <width> <height>]\n\t[-o|--output <filename>]\n", progName);
+   exit(0);
+}
+
+void printHelp2(char const *progName, char const *errorDescription)
+{
+   printHelp4(progName, errorDescription, "", "");
+}
+
+void printHelp1(char const *progName)
+{
+   printHelp2(progName, "");
+}
+
+void printHelp()
+{
+   printHelp1("RayTracingC");
+}
+
 void *rowThread(void *thArgsPtr)
 {
    threadArgs *thArgs = (threadArgs *)thArgsPtr;
@@ -32,9 +58,9 @@ void *rowThread(void *thArgsPtr)
    {
       for (int x = 0; x < width; ++x)
       {
-         float dx = (x-width / 2)/(float)(height / 2);
-         float dy = (y-(height / 2))/(float)(height / 2);
-         vec3 dir = plus(plus(times(thArgs->ex,dx),times(thArgs->ey,dy)),times(thArgs->ez,thArgs->fov));
+         float dx = (x - width / 2) / (float)(height / 2);
+         float dy = (y - (height / 2)) / (float)(height / 2);
+         vec3 dir = plus(plus(times(thArgs->ex, dx), times(thArgs->ey, dy)), times(thArgs->ez, thArgs->fov));
          // vec3 dir = {(x - width / 2) / (float)(height / 2), (y - (height / 2)) / (float)(height / 2), 1}; // aspect ratio respected
          // dir = minus(dir,origin);//fixed camera dir
          dir = normalized(dir);
@@ -52,15 +78,76 @@ void *rowThread(void *thArgsPtr)
 
 int main(int argc, char const *argv[])
 {
+   // DEFAULT SETTINGS
+   // mode is 'default' or a .obj path/filename
+   char mode[256] = "default";
+   char outputFileName[256] = "out.bmp";
    int trianglesOnly = 0;
-   normalizedSunDirection = normalized(sunDirection);
-   if (argc < 2 || strcmp(argv[1], "--help") == 0 || (argc != 2 && argc != 5 && argc != 8 && argc != 9 && argc != 11))
+   vec3 origin = {-4.75, -1.5, -4.75};
+   vec3 lookingAt = {0.9, -1.2, 1};
+   float fov = 1;
+
+   // READING PARAMS
+   for (unsigned int i = 1; i < argc; i++)
    {
-      printf("%s needs at least one param.\n", argv[0]);
-      printf("%s <default|path/to/file.obj> [posX posY posZ] [trackX trackY trackZ] [fov] [width height].\n", argv[0]);
-      exit(0);
+      if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
+         printHelp1(argv[0]);
+      else if (strcmp(argv[i], "--pos") == 0 || strcmp(argv[i], "-p") == 0)
+      {
+         if (argc - 1 < i + 3)
+            printHelp2(argv[0], "ERROR: --pos/-p takes 3 more params (float x,y,z)\n");
+         origin.x = atof(argv[i + 1]);
+         origin.y = atof(argv[i + 2]);
+         origin.z = atof(argv[i + 3]);
+         i += 3;
+      }
+      else if (strcmp(argv[i], "--track") == 0 || strcmp(argv[i], "-t") == 0)
+      {
+         if (argc - 1 < i + 3)
+            printHelp2(argv[0], "ERROR: --track/-t takes 3 more params (float x,y,z)\n");
+         lookingAt.x = atof(argv[i + 1]);
+         lookingAt.y = atof(argv[i + 2]);
+         lookingAt.z = atof(argv[i + 3]);
+         i += 3;
+      }
+      else if (strcmp(argv[i], "--fov") == 0 || strcmp(argv[i], "-f") == 0)
+      {
+         if (argc - 1 < i + 1)
+            printHelp2(argv[0], "ERROR: --fov/-i takes 1 more param (float f)\n");
+         fov = atof(argv[i + 1]);
+         i += 1;
+      }
+      else if (strcmp(argv[i], "--size") == 0 || strcmp(argv[i], "-s") == 0)
+      {
+         if (argc - 1 < i + 2)
+            printHelp2(argv[0], "ERROR: --size/-s takes 2 more params (int w,h)\n");
+         width = atoi(argv[i + 1]);
+         height = atoi(argv[i + 2]);
+         i += 2;
+      }
+      else if (strcmp(argv[i], "--input") == 0 || strcmp(argv[i], "-i") == 0)
+      {
+         if (argc - 1 < i + 1)
+            printHelp2(argv[0], "ERROR: --input/-i takes 1 more param (string path/to/filename.obj)\n");
+         strcpy(mode, argv[i + 1]);
+         i += 1;
+      }
+      else if (strcmp(argv[i], "--output") == 0 || strcmp(argv[i], "-o") == 0)
+      {
+         if (argc - 1 < i + 1)
+            printHelp2(argv[0], "ERROR: --output/-o takes 1 more param (string outImage.bmp)\n");
+         strcpy(outputFileName, argv[i + 1]);
+         i += 1;
+      }
+      else
+      {
+         printHelp4(argv[0], "ERROR: UNKNOWN ARGUMENT \"", argv[i], "\"\n");
+      }
    }
-   else if (strcmp(argv[1], "default") == 0) {
+
+   printf("Starting RayTracingC in %s mode", mode);
+   if (strcmp(mode, "default") == 0)
+   {
       printf("Parsing triangles...\n");
       parseTriangleFile("triangles.txt");
    }
@@ -68,37 +155,15 @@ int main(int argc, char const *argv[])
    {
       trianglesOnly = 1;
       printf("Loading obj...\n");
-      loadOBJTriangles(argv[1]);
-   }
-   // printAllTriangles();
-   vec3 origin = {-4.75, -1.5, -4.75};
-   if (argc >= 5) {
-      origin.x = atof(argv[2]);
-      origin.y = atof(argv[3]);
-      origin.z = atof(argv[4]);
-   }
-
-   vec3 lookingAt = {0.9,-1.2,1};
-   if (argc >= 8) {
-      lookingAt.x = atof(argv[5]);
-      lookingAt.y = atof(argv[6]);
-      lookingAt.z = atof(argv[7]);
-   }
-   float fov = 1;
-   if (argc >= 9) {
-      fov = atof(argv[8]);
-   }
-   if (argc >= 11) {
-      width = atoi(argv[9]);
-      height = atoi(argv[10]);
+      loadOBJTriangles(mode);
    }
 
    Color image[width * height];
-   
-   vec3 ez = normalized(minus(lookingAt,origin));
-   vec3 up = {0,-1,0};
-   vec3 ex = normalized(cross(ez,up));
-   vec3 ey = normalized(cross(ez,ex));
+   normalizedSunDirection = normalized(sunDirection);
+   vec3 ez = normalized(minus(lookingAt, origin));
+   vec3 up = {0, -1, 0};
+   vec3 ex = normalized(cross(ez, up));
+   vec3 ey = normalized(cross(ez, ex));
    /*
    vec3 dir={0,0,1};
    dir=normalized(dir);
@@ -106,17 +171,18 @@ int main(int argc, char const *argv[])
    rngState = 0;
    calcColor(ray,3);
    */
-
+   printf("Starting RENDERING...\n");
 #if defined(_WIN32) || (MULTITHREADED == 0)
    for (int y = 0; y < height; ++y)
    {
-      if(y%10==0)printf("[%4i/%i] Processing...\n",y,height);
+      if (y % 10 == 0)
+         printf("[%4i/%i] Processing...\n", y, height);
       for (int x = 0; x < width; ++x)
       {
-         //vec3 dir = {(x - width / 2) / (float)(height / 2), (y - (height / 2)) / (float)(height / 2), 1}; // aspect ratio respected
-         float dx = (x-width / 2)/(float)(height / 2);
-         float dy = (y-(height / 2))/(float)(height / 2);
-         vec3 dir = plus(plus(times(ex,dx),times(ey,dy)),times(ez,fov));//dx*ex + dy*ey + fov*ez;
+         // vec3 dir = {(x - width / 2) / (float)(height / 2), (y - (height / 2)) / (float)(height / 2), 1}; // aspect ratio respected
+         float dx = (x - width / 2) / (float)(height / 2);
+         float dy = (y - (height / 2)) / (float)(height / 2);
+         vec3 dir = plus(plus(times(ex, dx), times(ey, dy)), times(ez, fov)); // dx*ex + dy*ey + fov*ez;
          dir = normalized(dir);
          Ray ray = {origin, dir};
          rngState = x + y * width;
@@ -147,7 +213,7 @@ int main(int argc, char const *argv[])
    }
 
 #endif
-   stbi_write_bmp("out.bmp", width, height, 3, (void const *)image);
+   stbi_write_bmp(outputFileName, width, height, 3, (void const *)image);
 
    return 0;
 }
--- scene.h
+++ scene.h
@@ -19,11 +19,11 @@ static const int sphereCount = sizeof(spheres)/sizeof(Sphere);
 static Triangle *triangles=NULL; //to parse from triangles.txt
 static int triangleCount = 0;
 
-// static int const w = 128*1;//width
-// static int const h = 128*1;//height
+// static int const w = 128*1;//width // MOVED BACK TO MAIN.C
+// static int const h = 128*1;//height // MOVED BACK TO MAIN.C
 static int const accumulationCount = 100*40;//1000 = ok
-static int const maxBounce = 10;
-static vec3 const sunDirection = {-30,-85,100};
+// static int const maxBounce = 10; // MOVED BACK TO MAIN.C
+// static vec3 const sunDirection = {-30,-85,100}; // MOVED BACK TO MAIN.C
 static vec3 const SkyColorHorizon = {1,1,1};
 static vec3 const SkyColorZenith = {0.263,0.969,0.871};//{.5,.5,1};
 static vec3 const GroundColor = {.66,.66,.66};
@@ -32,8 +32,6 @@ static float const SunIntensity = .75;
 
 
 static vec3 normalizedSunDirection;
-// static int const halfW = w/2;
-// static int const halfH = h/2;
 static unsigned int rngState;
 
 #define EPSILON 0.001
